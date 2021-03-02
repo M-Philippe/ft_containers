25d2b0962f2561aa074dad0250c7a1bacc35464d
M-Philippe <m.philippe59@gmail.com>
2021-03-02 17:39:36 +0100

    first commit

---
+++ ft_list/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+#include "ft_list.hpp"
+
+int main() {
+
+	ft::list<int> a(5, 10);
+	a.TMP_Display();
+}
+
+	// std::list<int> lst(25, 10);
+	// for (int i = 0; i < 30; i++) {
+	// 	lst.push_back(i);
+	// }
+	// std::list<int>::iterator it = lst.begin();
+	// while (it != lst.end())
+	// 	std::cout << *it++ << std::endl;
